44102c1f69f39e1975098ef78d76e99c317431b0
Vlad Zinevich <vladisznv@gmail.com>
2021-08-09 16:06:25 +0300

    Initial Commit

---
+++ Contacts_app/MainTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabBarController.swift
+//  Contacts_app
+//
+//  Created by Vlad Zinevich on 09.08.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Contacts_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts_app
+//
+//  Created by Vlad Zinevich on 09.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
