5b999a026c17ff797250131641e0ad9557d0b6d7
Vlad Zinevich <vladisznv@gmail.com>
2021-08-11 09:36:31 +0300

    add tab bar and navigation bar for contacts screen

--- Contacts_app/ContactsViewController.swift
+++ Contacts_app/ContactsViewController.swift
@@ -8,13 +8,45 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
+    
+    let addBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonItemTapped))
+    let groupsBarButtonItem = UIBarButtonItem(title: "Groups", style: .plain, target: self, action: #selector(addBarButtonItemTapped))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
+        setupNavigationBar()
     }
-
-
+    
+    private func setupNavigationBar() {
+        let searchController = UISearchController()
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        
+        navigationItem.leftBarButtonItem = groupsBarButtonItem
+        navigationItem.rightBarButtonItem = addBarButtonItem
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Contacts"
+    }
 }
 
+// MARK: - Actions
+extension ContactsViewController {
+    @objc func addBarButtonItemTapped() {
+        print(#function)
+    }
+    @objc func groupsBarButtonItemTapped() {
+        print(#function)
+    }
+}
+
+extension ContactsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
